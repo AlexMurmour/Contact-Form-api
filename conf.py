e0477af995183e8cf5d9fa837cb52c9fb948aa16
AlexMurmour <33135290+AlexMurmour@users.noreply.github.com>
2019-06-13 21:50:24 +0300

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,6 @@
+EMAIL_BACKEND = "anymail.backends.sendgrid.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "example@gmail.com"
+EMAIL_HOST_PASSWORD = "your password"
+SENDGRID_API_KEY = "your key"
