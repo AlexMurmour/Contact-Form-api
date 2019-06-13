502fd3473021fb0b6b17b8f4c72e649e57f635b5
AlexMurmour <33135290+AlexMurmour@users.noreply.github.com>
2019-06-13 21:41:12 +0300

    Update models.py

--- models.py
+++ models.py
@@ -5,20 +5,8 @@ from django.db import models
 class Contact(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     email = models.EmailField()
-    tags = models.TextField()
-    geolocation = models.TextField()
-    coin = models.CharField(max_length=30)
-    plan = models.CharField(max_length=30)
-    place = models.CharField(max_length=80)
-    antitag = models.TextField()
-    instuser = models.CharField(max_length=30)
-    instpass = models.CharField(max_length=30)
-    # time = models.DateTimeField(auto_now=True, blank=true)
-    # confirm = models.BooleanField(default=False)
-
-    # def numcontact(self):
-    #     all_ratings = Contact.object.filter(id(User))
-    #     return all_ratings
+    text = models.TextField()
+    
     def __str__(self):
         return Contact.user, Contact.email, Contact.instuser
 
